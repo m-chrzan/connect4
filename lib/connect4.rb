51237860c010cbfe6d07d092fb3da6da6b994117
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2016-08-27 20:20:17 -0400

    Implement game
    - #drop (raises ColumnFilled)
    - #current_player
    - #status

--- lib/connect4.rb
+++ lib/connect4.rb
@@ -4,8 +4,29 @@ class Game
     def initialize
         @status = :continue
         @current_player = :player1
+        @board = Board.new
+    end
+
+    def drop column
+        @board.drop(column, @current_player)
+        update_status column
+        @current_player = @current_player == :player1 ? :player2 : :player1
+    end
+
+    def update_status column
+        candidates = @board.get_row_col_diags column
+        if candidates.any? { |cand| cand =~ /([12])\1\1\1/ }
+            @status = (@current_player.to_s + "won").to_sym
+        elsif @board.filled?
+            @status = :draw
+        end
+    end
+
+    def to_s
+        @board.to_s
     end
 end
+
 class Board
     attr_reader :columns
 
@@ -85,6 +106,30 @@ class Board
 
     def filled?
         @pieces >= 42
+    end
+
+    def piece_to_char piece
+        case(piece)
+        when :player1
+            '○'
+        when :player2
+            '●'
+        else
+            ' '
+        end
+    end
+
+    def to_s
+        string = " 1 2 3 4 5 6 7 \n"
+        (0..5).to_a.reverse.each do |row|
+            string << "|"
+            @columns.each do |col|
+                string << piece_to_char(col[row]) + "|"
+            end
+            string << "\n"
+        end
+
+        string
     end
 end
 
